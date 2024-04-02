429408abffe9f0354257b4eafa3b065604b15fe0
meikhaill <meikhailmajor9@gmail.com>
2024-04-02 13:23:34 -0600

    progress

--- lib/bst.rb
+++ lib/bst.rb
@@ -3,12 +3,31 @@ require_relative 'node.rb'
 class BinarySearchTree
 
   attr_reader :root
-  
+
   def initialize 
     @root = nil
   end
 
-  def insert(score, name)
+  def insert(curr_node, node)
+    if @root = nil
+      @root = node
+      curr_node = @root
+    end
     
+    if node.score == curr_node
+      return node
+    elsif 
+      node.score < curr_node.score
+      curr_node.left = node
+      curr_node = curr_node.left
+    elsif 
+      node.score > curr_node.score
+      curr_node.right = node
+      curr_node = curr_node.right
+    else
+      puts "failure"
+    end
+
+
   end
 end
