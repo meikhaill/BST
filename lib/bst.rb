9d5531a779572d07bc178b1429d5a1c47faff8cb
meikhaill <meikhailmajor9@gmail.com>
2024-04-03 09:52:29 -0600

    working binary search tree, adding additional methods

--- lib/bst.rb
+++ lib/bst.rb
@@ -10,6 +10,7 @@ class BinarySearchTree
 
   def insert(score,name)
     @root = insert_recursively(@root, score, name)
+    puts "root is currently #{@root}"
   end
 
   def insert_recursively(curr_node, score, name)
@@ -19,24 +20,37 @@ class BinarySearchTree
     end
 
     if curr_node == nil
-      curr_node = Node.new(score, name)
+      return Node.new(score, name)
     end
-    
-    
     
     if score < curr_node.score 
-      curr_node.left = Node.new(score, name)
-      curr_node = curr_node.left
-      puts "new node on left"
-      puts curr_node
-    
+      if curr_node.left == nil 
+        curr_node.left = Node.new(score, name)
+      elsif 
+        curr_node.left = insert_recursively(curr_node.left, score, name)
+      end
     elsif score > curr_node.score
-      curr_node.right = Node.new(score, name)
-      curr_node = curr_node.right
-      puts "new node on right"
-      puts curr_node
-    
+      if curr_node.right == nil
+        curr_node.right = Node.new(score, name)
+      elsif 
+        curr_node.right = insert_recursively(curr_node.right, score, name)
+      end
     end
+    
+    
+    # # if score < curr_node.score 
+    # #   curr_node.left = insert_recursively(curr_node.left, score, name)
+    # #   curr_node = curr_node.left
+    # #   puts "new node on left"
+    # #   puts curr_node.left
+    
+    # elsif score > curr_node.score
+    #   curr_node.right = Node.new(score, name)
+    #   curr_node = curr_node.right
+    #   puts "new node on right"
+    #   puts curr_node
+    
+    # end
     curr_node
   end
 end
